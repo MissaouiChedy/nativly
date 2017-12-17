f5d3d953725e06b4ace2126812421f1f39f4c840
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-12-17 10:21:04 +0100

    Added the benchmark and add functions

--- lib/nativly.ex
+++ lib/nativly.ex
@@ -5,8 +5,38 @@ defmodule Nativly do
     :erlang.load_nif('./nativly', 0)
   end
   
+  def benchmark do
+    n = 1_000_000
+    a = vector(n)
+    b = vector(n)
+
+    {duration_ex, _} = :timer.tc(fn -> exdot(a, b) end)
+    {duration_cpp, _} = :timer.tc(fn -> dot(a, b) end)
+    
+    IO.puts "For N = #{n}:"
+    IO.puts "=============" 
+    IO.puts "Elixir took #{duration_ex / 1_000_000}s"
+    IO.puts "Native took #{duration_cpp / 1_000_000}s"
+  end
+  
   def dot(_a, _b) do
     raise "NIF dot/2 not implemented"
+  end
+
+  def exdot(a, b) do
+    Enum.zip(a, b)
+    |> Enum.map(fn {ea, eb} -> ea * eb end)
+    |> Enum.reduce(&Kernel.+/2)
+  end
+  
+  def add(_a, _b) do
+    raise "NIF add/2 not implemented"
+  end
+
+  def vector(n) do
+    0..n
+    |> Enum.map(fn x -> x / 1 end)
+    |> Enum.shuffle()
   end
 
   def rand_list(n) do
@@ -15,5 +45,4 @@ defmodule Nativly do
     |> Enum.map(fn x -> x /1 end)
     |> Enum.shuffle()
   end
-
 end
--- native_lib/nativly.cpp
+++ native_lib/nativly.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-double dot(const vector<double> &a, const vector<double> &b) {
+double dot(const vector<double> &a, const vector<double> &b) 
+{
     double result = 0.0d;
     for (long i = 0; i < a.size(); ++i) {
         result += a[i] * b[i];
@@ -11,7 +12,13 @@ double dot(const vector<double> &a, const vector<double> &b) {
     return result;
 }
 
-inline bool fillVector(ErlNifEnv* env, ERL_NIF_TERM listTerm, vector<double> &result) {
+int add (int a, int b)
+{
+    return a + b;
+}
+
+inline bool fillVector(ErlNifEnv* env, ERL_NIF_TERM listTerm, vector<double> &result) 
+{
     unsigned int length = 0;
     
     if (!enif_get_list_length(env, listTerm, &length)) {
@@ -52,8 +59,26 @@ ERL_NIF_TERM dot_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     return enif_make_double(env, result);
 }
 
-ErlNifFunc nif_funcs[] = {
+ERL_NIF_TERM add_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    int a = 0;
+    int b = 0;
+    
+    if (!enif_get_int(env, argv[0], &a)) {
+        return enif_make_badarg(env);
+    }
+    if (!enif_get_int(env, argv[1], &b)) {
+        return enif_make_badarg(env);
+    }
+    
+    int result = add(a, b);
+    return enif_make_int(env, result);
+}
+
+ErlNifFunc nif_funcs[] = 
+{
     {"dot", 2, dot_nif},
+    {"add", 2, add_nif},
 };
 
 ERL_NIF_INIT(Elixir.Nativly, nif_funcs, nullptr, nullptr, nullptr, nullptr);
